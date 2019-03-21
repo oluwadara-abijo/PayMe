796c3590da3b0cd5d7a679857f232d51ae4ac1b8
Oluwadara Abijo <oluwadara.abijo@gmail.com>
2019-03-21 20:57:43 +0100

    Initialize Paystack SDK

---
+++ app/src/main/java/MyApplication.java
@@ -0,0 +1,14 @@
+import android.annotation.SuppressLint;
+import android.app.Application;
+
+import co.paystack.android.PaystackSdk;
+
+@SuppressLint("Registered")
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //Initialize Paystack SDK
+        PaystackSdk.initialize(getApplicationContext());
+    }
+}
